2ca08a90cfdc57b3dd370541054ac14a06b0a83d
ko5 <kotyana1331@gmail.com>
2021-02-03 10:51:50 +0900

    fix: add 'I' to prefix of interface name

--- lib/domain/service/interface/handle_waiting_minos.dart
+++ lib/domain/service/interface/handle_waiting_minos.dart
@@ -1,7 +1,7 @@
 import 'package:re_tetris/domain/model/mino.dart';
 import 'package:re_tetris/domain/model/waiting_mino_info.dart';
 
-abstract class HandleWaitingMinos {
+abstract class IHandleWaitingMinos {
   WaitingMinoInfo holdMino();
 
   Mino refresh(Mino mino);
