2f9eeee4f6eb292a35b2fe56887f8dcc3a66e9c6
ko5 <kotyana1331@gmail.com>
2021-02-03 12:29:00 +0900

    improve: supported field edge to blockVvalidator

--- lib/domain/service/impl/block_validation.dart
+++ lib/domain/service/impl/block_validation.dart
@@ -1,7 +1,13 @@
+import 'package:re_tetris/constants.dart';
 import 'package:re_tetris/domain/model/block.dart';
 import 'package:re_tetris/domain/service/interface/block_validation.dart';
 
 class BlockValidation implements IBlockValidation {
-  bool canPutOn(Block block, List<Block> filled) => filled.any(
-      (placedBlock) => placedBlock.x == block.x && placedBlock.y == block.y);
+  bool canPutOn(Block block, List<Block> filled) => filled.any((placedBlock) {
+        if (block.x >= FIELD_WIDTH) return false;
+        if (block.x < 0) return false;
+        if (block.y < 0) return false;
+
+        return placedBlock.x == block.x && placedBlock.y == block.y;
+      });
 }
