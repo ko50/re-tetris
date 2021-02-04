2bdbfb59e38d24b55b82a3d4a2adf0d6013f8fe1
ko5 <kotyana1331@gmail.com>
2021-02-04 23:54:38 +0900

    add: cast Blocks->Placement and Placement->Blocks

--- lib/domain/service/interface/rotate_mino.dart
+++ lib/domain/service/interface/rotate_mino.dart
@@ -1,6 +1,11 @@
 import 'package:re_tetris/domain/enum/direction.dart';
+import 'package:re_tetris/domain/model/block.dart';
 import 'package:re_tetris/domain/model/mino.dart';
 
 abstract class IRotateMino {
   Mino rotateMino(Mino mino, RotateDirection direction);
+
+  List<List<int>> convertBlocks(Mino mino);
+
+  List<Block> convertPlacement(List<List<int>> placement);
 }
