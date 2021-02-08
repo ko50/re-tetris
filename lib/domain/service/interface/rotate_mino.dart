c611b0457641bc30a6ea8e6e40cfe9251b83f694
ko5 <kotyana1331@gmail.com>
2021-02-08 13:30:22 +0900

    change: arguments of convertPlacement

--- lib/domain/service/interface/rotate_mino.dart
+++ lib/domain/service/interface/rotate_mino.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import 'package:re_tetris/domain/enum/direction.dart';
 import 'package:re_tetris/domain/model/block.dart';
 import 'package:re_tetris/domain/model/mino.dart';
@@ -7,5 +9,9 @@ abstract class IRotateMino {
 
   List<List<int>> convertBlocks(Mino mino);
 
-  List<Block> convertPlacement(List<List<int>> placement);
+  List<Block> convertPlacement(
+    List<List<int>> placement,
+    Color color,
+    Offset corner,
+  );
 }
