320075ea52139362d514721b1b9c4f0b6973e864
ko5 <kotyana1331@gmail.com>
2021-02-10 22:56:17 +0900

    fix: remove side effects of mobilizemino

--- lib/domain/service/impl/mobilize_mino.dart
+++ lib/domain/service/impl/mobilize_mino.dart
@@ -3,12 +3,30 @@ import 'package:re_tetris/domain/model/mino.dart';
 import 'package:re_tetris/domain/service/interface/mobilize_mino.dart';
 
 class MobilizeMino implements IMobilizeMino {
-  Mino moveRight(Mino mino) =>
-      mino..blocks.map<Block>((block) => block..cordinate.x += 1).toList();
+  Mino moveRight(Mino mino) {
+    final List<Block> blocks =
+        mino.blocks.map<Block>((b) => Block.from(b)).toList();
 
-  Mino moveLeft(Mino mino) =>
-      mino..blocks.map<Block>((block) => block..cordinate.x -= 1).toList();
+    blocks.forEach((block) => block.cordinate.x += 1);
 
-  Mino down(Mino mino) =>
-      mino..blocks.map<Block>((block) => block..cordinate.y -= 1).toList();
+    return Mino.from(mino)..blocks = blocks;
+  }
+
+  Mino moveLeft(Mino mino) {
+    final List<Block> blocks =
+        mino.blocks.map<Block>((b) => Block.from(b)).toList();
+
+    blocks.forEach((block) => block.cordinate.x -= 1);
+
+    return Mino.from(mino)..blocks = blocks;
+  }
+
+  Mino down(Mino mino) {
+    final List<Block> blocks =
+        mino.blocks.map<Block>((b) => Block.from(b)).toList();
+
+    blocks.forEach((block) => block.cordinate.y -= 1);
+
+    return Mino.from(mino)..blocks = blocks;
+  }
 }
