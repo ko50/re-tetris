9072290ab2cb9a7e409bbaae1f0caf89b26d60f8
ko5 <kotyana1331@gmail.com>
2021-02-10 23:09:52 +0900

    feat: test of mobilize mino

---
+++ test/service/mobilize_mino_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:re_tetris/domain/enum/tetromino.dart';
+import 'package:re_tetris/domain/model/mino.dart';
+import 'package:re_tetris/domain/service/impl/mobilize_mino.dart';
+
+void main() {
+  final MobilizeMino mobilizeMino = MobilizeMino();
+
+  late Mino mino;
+
+  Mino movedMino;
+
+  setUp(() {
+    mino = Mino(TetroMino.T);
+  });
+
+  test('Move to right', () {
+    movedMino = mobilizeMino.moveRight(mino);
+    expect(
+      movedMino.blocks.map<int>((b) => b.cordinate.x),
+      [5, 4, 5, 6],
+    );
+  });
+
+  test('Move to left', () {
+    movedMino = mobilizeMino.moveLeft(mino);
+    expect(
+      movedMino.blocks.map<int>((b) => b.cordinate.x),
+      [3, 2, 3, 4],
+    );
+  });
+
+  test('Move to under', () {
+    movedMino = mobilizeMino.down(mino);
+    expect(
+      movedMino.blocks.map<int>((b) => b.cordinate.y),
+      [19, 18, 18, 18],
+    );
+  });
+}
