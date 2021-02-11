c787d804e2b20766cdf57633e4e00de512df40cf
ko5 <kotyana1331@gmail.com>
2021-02-11 13:10:31 +0900

    change: interface of OperateMino's member

--- lib/application/usecase/interface/operate_mino.dart
+++ lib/application/usecase/interface/operate_mino.dart
@@ -1,7 +1,14 @@
 import 'package:re_tetris/domain/enum/direction.dart';
 import 'package:re_tetris/domain/model/mino.dart';
+import 'package:re_tetris/domain/service/interface/mobilize.dart';
+import 'package:re_tetris/domain/service/interface/rotate.dart';
 
 abstract class IOperateMino {
+  IRotate rotate;
+  IMobilize mobilize;
+
+  IOperateMino(this.rotate, this.mobilize);
+
   Mino moveMino(Mino mino, MoveDirection direction);
 
   Mino rotateMino(Mino mino, RotateDirection direction);
