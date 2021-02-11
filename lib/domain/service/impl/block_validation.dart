0937c1d41d1e738a38c4b54ef2978e945d0d593d
ko5 <kotyana1331@gmail.com>
2021-02-11 20:14:17 +0900

    change: add validator of mino

--- lib/domain/service/impl/block_validation.dart
+++ lib/domain/service/impl/block_validation.dart
@@ -1,13 +1,18 @@
 import 'package:re_tetris/constants.dart';
 import 'package:re_tetris/domain/model/block.dart';
+import 'package:re_tetris/domain/model/mino.dart';
 import 'package:re_tetris/domain/service/interface/block_validation.dart';
 
 class BlockValidation implements IBlockValidation {
-  bool canPutOn(Block block, List<Block> filled) => filled.any((placedBlock) {
+  bool canPutBlock(Block block, List<Block> placedBlocks) =>
+      placedBlocks.any((placedBlock) {
         if (block.cordinate.x >= FIELD_WIDTH) return false;
         if (block.cordinate.x < 0) return false;
         if (block.cordinate.y < 0) return false;
 
         return placedBlock.cordinate != block.cordinate;
       });
+
+  bool canPutMino(Mino mino, List<Block> placedBlocks) =>
+      mino.blocks.every((block) => canPutBlock(block, placedBlocks));
 }
