21cb3e5ce7b41c18ba336c044102217c9c61751b
ko5 <kotyana1331@gmail.com>
2021-02-12 12:16:19 +0900

    feat: interface of field controller

---
+++ lib/application/controller/interface/field_controller.dart
@@ -0,0 +1,17 @@
+import 'package:re_tetris/application/usecase/operate_mino.dart';
+import 'package:re_tetris/domain/model/block.dart';
+import 'package:re_tetris/domain/model/mino.dart';
+import 'package:re_tetris/domain/model/waiting_mino_info.dart';
+
+abstract class IFieldController {
+  late final OperateMino minoOperator;
+  late List<Block> placedBlocks;
+  late Mino operatingMino;
+  late WaitingMinoInfo waitingMinoInfo;
+
+  void hold();
+
+  void onTick();
+
+  void putMino();
+}
