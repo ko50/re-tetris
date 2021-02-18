cdcdc215723d2e858af430f8c41ac20ad2b3cc2b
ko5 <kotyana1331@gmail.com>
2021-02-18 23:51:39 +0900

    change: rename manageWaitingMinos

--- lib/domain/service/impl/manage_minos.dart
+++ lib/domain/service/impl/manage_minos.dart
@@ -1,7 +1,7 @@
 import 'package:re_tetris/domain/enum/tetromino.dart';
 import 'package:re_tetris/domain/model/mino_administrator.dart';
 import 'package:re_tetris/domain/model/mino.dart';
-import 'package:re_tetris/domain/service/interface/manage_waiting_minos.dart';
+import 'package:re_tetris/domain/service/interface/manage_minos.dart';
 
 class ManageMinos implements IManageMinos {
   MinoAdministrator supplyNextMinos(MinoAdministrator current) {
