7a69b00d801ba4cbe4310642b7acf0915e414b68
ko5 <kotyana1331@gmail.com>
2021-02-20 01:39:37 +0900

    change: modify constant values

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,10 @@ const int FIELD_WIDTH = 10;
 
 const int FIELD_HEIGHT = 20;
 
-const int GAME_TICK = 1000 ~/ 60;
+const int GAME_TICK = 1000000 ~/ 60;
 
-const Duration GAME_DURATION = Duration(milliseconds: GAME_TICK);
+const Duration GAME_DURATION = Duration(microseconds: GAME_TICK);
+
+const int DOWN_SPEED = 500;
+
+const int LOCKDOWN_MARGIN = 500000;
