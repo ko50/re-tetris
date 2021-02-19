5741eec6496d9e9b372476f4e5aa7b71696709ac
ko5 <kotyana1331@gmail.com>
2021-02-19 17:16:51 +0900

    impl: implement field_controller

--- lib/application/controller/impl/field_controller.dart
+++ lib/application/controller/impl/field_controller.dart
@@ -1,30 +1,109 @@
 import 'package:re_tetris/application/controller/interface/field_controller.dart';
 import 'package:re_tetris/application/usecase/operate_mino.dart';
+import 'package:re_tetris/constants.dart';
 import 'package:re_tetris/domain/enum/direction.dart';
 import 'package:re_tetris/domain/enum/tetromino.dart';
 import 'package:re_tetris/domain/model/block.dart';
+import 'package:re_tetris/domain/model/cordinate.dart';
 import 'package:re_tetris/domain/model/mino.dart';
-import 'package:re_tetris/domain/model/waiting_mino_info.dart';
+import 'package:re_tetris/domain/model/mino_administrator.dart';
+import 'package:re_tetris/domain/service/interface/manage_minos.dart';
 
 class FieldController implements IFieldController {
-  late final OperateMino minoOperator;
-  late Mino operatingMino;
+  final OperateMino minoOperator;
+
+  IManageMinos manageMinos;
+
   List<Block> placedBlocks = [];
-  WaitingMinoInfo waitingMinoInfo = WaitingMinoInfo(
-    nextMinos: List.from(TetroMino.values)..shuffle(),
-  );
+  MinoAdministrator minoAdministrator;
+  int lockDownMarginRemain = 15;
+  int lockDownMargin = 500;
 
-  FieldController({required this.minoOperator}) {}
+  FieldController({
+    required List<TetroMino> nextMinos,
+    required this.minoOperator,
+    required this.manageMinos,
+  }) : this.minoAdministrator = MinoAdministrator(nextMinos: nextMinos);
 
-  void hold() {}
+  void _resetMargin() {
+    lockDownMarginRemain = 15;
+    lockDownMargin = 500;
+  }
 
-  void move(MoveDirection direction) {}
+  void hold() {
+    minoAdministrator = manageMinos.holdMino(minoAdministrator);
+    _resetMargin();
+  }
 
-  void rotate(RotateDirection direction) {}
+  void move(MoveDirection direction) {
+    Cordinate before =
+        Cordinate.from(minoAdministrator.operatingMino.cornerCordinate);
 
-  void onTick() {}
+    minoAdministrator.operatingMino = minoOperator.moveMino(
+      minoAdministrator.operatingMino,
+      direction,
+      placedBlocks,
+    );
 
-  void putMino() {}
+    Cordinate moved =
+        Cordinate.from(minoAdministrator.operatingMino.cornerCordinate);
+
+    if (moved.y < before.y)
+      _resetMargin();
+    else if (moved != before) {
+      lockDownMargin = 500;
+      lockDownMarginRemain -= 1;
+    }
+
+    if (direction == MoveDirection.Up) put();
+  }
+
+  void rotate(RotateDirection direction) {
+    Cordinate before =
+        Cordinate.from(minoAdministrator.operatingMino.cornerCordinate);
+
+    minoAdministrator.operatingMino = minoOperator.rotateMino(
+      minoAdministrator.operatingMino,
+      direction,
+      placedBlocks,
+    );
+
+    Cordinate moved =
+        Cordinate.from(minoAdministrator.operatingMino.cornerCordinate);
+
+    if (moved.y < before.y)
+      _resetMargin();
+    else if (moved != before) {
+      lockDownMargin = 500;
+      lockDownMarginRemain -= 1;
+    }
+  }
+
+  void onTick() {
+    lockDownMargin -= GAME_TICK;
+
+    Cordinate before =
+        Cordinate.from(minoAdministrator.operatingMino.cornerCordinate);
+
+    minoAdministrator.operatingMino = minoOperator.moveMino(
+      minoAdministrator.operatingMino,
+      MoveDirection.Down,
+      placedBlocks,
+    );
+
+    Cordinate moved =
+        Cordinate.from(minoAdministrator.operatingMino.cornerCordinate);
+
+    if (moved.y < before.y)
+      _resetMargin();
+    else if (lockDownMargin <= 0) put();
+  }
+
+  void put() {
+    minoAdministrator = manageMinos.putMino(minoAdministrator);
+    _lineClear();
+    _resetMargin();
+  }
 
   void _lineClear() {}
 }
