d676de2b17e23dd9b7cd47f5f18f0ed5fe5a86d3
ko5 <kotyana1331@gmail.com>
2021-02-20 03:16:04 +0900

    add: presenter of single playing screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,9 +2,9 @@ const int FIELD_WIDTH = 10;
 
 const int FIELD_HEIGHT = 20;
 
-const int GAME_TICK = 1000000 ~/ 60;
+const int GAME_TICK = 100;
 
-const Duration GAME_DURATION = Duration(microseconds: GAME_TICK);
+const Duration GAME_DURATION = Duration(milliseconds: GAME_TICK);
 
 const int DOWN_SPEED = 500;
 
