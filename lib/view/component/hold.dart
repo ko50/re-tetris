428939a129e150d582afb5170367e4110e4ed237
ko5 <kotyana1331@gmail.com>
2021-02-20 14:07:10 +0900

    add: widget of holded mino

---
+++ lib/view/component/hold.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:re_tetris/domain/model/block.dart';
+import 'package:re_tetris/view/component/painter.dart';
+
+class Hold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<Block> blocks = context.watch<ValueNotifier<List<Block>>>().value;
+
+    return Container(
+      color: Colors.black,
+      child: Center(
+        child: CustomPaint(
+          painter: Painter(blocks),
+        ),
+      ),
+    );
+  }
+}
