2591613462f599c012526bbd945e90df391efd2d
ko5 <kotyana1331@gmail.com>
2021-02-20 14:48:06 +0900

    fix: separate mino displayer

--- lib/view/component/hold.dart
+++ lib/view/component/hold.dart
@@ -3,23 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:re_tetris/domain/model/block.dart';
-import 'package:re_tetris/view/component/painter.dart';
+import 'package:re_tetris/view/component/mino_displayer.dart';
 
 class Hold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Block> blocks = context.watch<ValueNotifier<List<Block>>>().value;
 
-    return AspectRatio(
-      aspectRatio: 2 / 3,
-      child: Container(
-        color: Colors.black,
-        child: Center(
-          child: CustomPaint(
-            painter: Painter(blocks),
-          ),
-        ),
-      ),
-    );
+    return MinoDisplayer(blocks);
   }
 }
---
+++ lib/view/component/mino_displayer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:re_tetris/domain/model/block.dart';
+import 'package:re_tetris/view/component/painter.dart';
+
+class MinoDisplayer extends StatelessWidget {
+  final List<Block> blocks;
+
+  MinoDisplayer(this.blocks);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2 / 3,
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: CustomPaint(
+            painter: Painter(blocks),
+          ),
+        ),
+      ),
+    );
+  }
+}
